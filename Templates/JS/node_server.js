c98b891309ed46f73ac76d8727035b1fe6dad97f
ttoe05 <terrillt15@gmail.com>
2019-02-24 20:48:45 -0500

    Adding node server and Bootstrap

---
+++ Templates/JS/node_server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (req, resp) {
+    if (req.url === "/") {
+        fs.readFile("../html/Home.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+
+            resp.end();
+        });
+    }
+});
+//5.
+server.listen(8000);
