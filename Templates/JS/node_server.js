e3aaa95364b006e9e90526b40b9577c12d7fecd6
ttoe05 <terrillt15@gmail.com>
2019-04-06 14:53:53 -0400

    Added video to home page

--- Templates/JS/node_server.js
+++ Templates/JS/node_server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function (req, resp) {
     if (req.url === "/") {
-        fs.readFile("../html/Home.html", function (error, pgResp) {
+        fs.readFile("../html/", function (error, pgResp) {
             if (error) {
                 resp.writeHead(404);
                 resp.write('Contents you are looking are Not Found');
